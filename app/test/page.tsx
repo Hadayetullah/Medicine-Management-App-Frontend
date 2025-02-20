b59fd3e791f06bc4adb25beaa8b2591ea9c12f30
Hadayetullah <hadayetullah30@gmail.com>
2025-02-20 22:22:12 +0600

    Little update

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,6 +3,7 @@ export default function TestEnvPage() {
     <div className="mt-[100px] text-center">
       <p>API Base URL: {process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}</p>
       <p>Socket Base URL: {process.env.NEXT_PUBLIC_BACKEND_SOCKET_BASE_URL}</p>
+      <p>Socket Base URL: {process.env.NODE_ENV}</p>
     </div>
   );
 }
